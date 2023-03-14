8b33560954a3dabff1b99afb4a011d4c9d93511b
mabgcm <bugucam@gmail.com>
2023-03-14 01:08:29 -0400

    rakuten remove mt

--- src/Footer.js
+++ src/Footer.js
@@ -39,7 +39,7 @@ const Footer = () => {
                                     <i className="bx bx-chevron-right"></i> <a href="faq">Những câu hỏi thường gặp</a>
                                 </li>
 
-                                <Link to="http://click.linksynergy.com/fs-bin/click?id=bZNGlZk1qGY&offerid=311675&type=3&subid=0&LSNSUBSITE=LSNSUBSITE" className='text-center mt-5'>Rakuten Advertising Welcome Program</Link><img style={{ border: 0, width: 1, height: 1 }} src="http://ad.linksynergy.com/fs-bin/show?id=bZNGlZk1qGY&bids=311675&type=3&subid=0" />
+                                <Link to="http://click.linksynergy.com/fs-bin/click?id=bZNGlZk1qGY&offerid=311675&type=3&subid=0&LSNSUBSITE=LSNSUBSITE" >Rakuten Advertising Welcome Program</Link><img style={{ border: 0, width: 1, height: 1 }} src="http://ad.linksynergy.com/fs-bin/show?id=bZNGlZk1qGY&bids=311675&type=3&subid=0" />
 
 
                             </ul>
